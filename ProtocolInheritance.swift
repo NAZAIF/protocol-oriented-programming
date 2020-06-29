f84410efd0e4afb4fab7ba80b2e597130c901fd9
Moideen Nazaif VM <nazaifmoid@gmail.com>
2020-06-29 13:33:03 +0530

    Defining default behaviour of Protocols using extensions

--- ProtocolInheritance.swift
+++ ProtocolInheritance.swift
@@ -11,23 +11,7 @@ protocol TaggedPersistable: Taggable, CustomStringConvertible, Equatable {
     func persist(to url: URL) throws
 }
 
-protocol TaggedEncodable: Taggable {
-    var base64: String { get}
-}
-
-struct MyData: Taggable {
-   
-    var tag: String
-    
-    var data: Data
-    
-    init(tag: String, data: Data) {
-        self.tag = tag
-        self.data = data
-    }
-}
-
-extension MyData: TaggedPersistable {
+extension TaggedPersistable {
     init(tag: String, contentsOf url: URL) throws {
         let data = try Data(contentsOf: url)
         self.init(tag: tag, data: data)
@@ -38,9 +22,25 @@ extension MyData: TaggedPersistable {
     }
 }
 
-extension MyData: TaggedEncodable {
+protocol TaggedEncodable: Taggable {
+    var base64: String { get}
+}
+
+extension TaggedEncodable {
     var base64: String {
         return self.data.base64EncodedString()
+    }
+}
+
+struct MyData: TaggedPersistable, TaggedEncodable {
+    
+    var tag: String
+    
+    var data: Data
+    
+    init(tag: String, data: Data) {
+        self.tag = tag
+        self.data = data
     }
 }
 
@@ -50,3 +50,21 @@ extension MyData: CustomStringConvertible {
     }
 }
 
+struct PersistableData: TaggedPersistable {
+    var tag: String
+    
+    var data: Data
+    
+    var description: String {
+        return "PersistableData(\(tag))"
+    }
+    
+    init(tag: String, data: Data) {
+        self.tag = tag
+        self.data = data
+    }
+}
+
+let p = PersistableData(tag: "34", data: Data(repeating: 1, count: 10))
+try? p.persist(to: URL(string: "<enter-some-url>")!)
+ 
